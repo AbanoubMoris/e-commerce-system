12ecdc5506620b03a0b4a5769094b5b42418deae
AbanobMoris <abanoub.moris1@gmail.com>
2023-05-15 04:59:57 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # e-commerce-system
+
+## first cd to the API folder
+## dotnet ef migrations add initialCreate -o Data/Migrations -p ..\Infrastructure
+## dotnet ef database update
---
+++ e-Commerce-System/e-Commerce-System/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace e_Commerce_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _productRepository.GetProductsAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            var product = await _productRepository.GetProductByIdAsync(id);
+            return Ok(product);
+        }
+    }
+}
